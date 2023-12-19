c51113ddce5d40c9844a5e16e29ddb4ef1f7f13e
Lã Thành Trung <117911517+lttthedevtmu@users.noreply.github.com>
2023-12-19 22:36:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,222 @@
+const videoContainer = document.querySelector("#videos");
+
+const vm = new Vue({
+  el: "#app",
+  data: {
+    userToken: "",
+    roomId: "",
+    roomToken: "",
+    room: undefined,
+    callClient: undefined
+  },
+  computed: {
+    roomUrl: function() {
+      return `https://${location.hostname}?room=${this.roomId}`;
+    }
+  },
+  async mounted() {
+    api.setRestToken();
+
+    const urlParams = new URLSearchParams(location.search);
+    const roomId = urlParams.get("room");
+    if (roomId) {
+      this.roomId = roomId;
+
+      await this.join();
+    }
+  },
+  methods: {
+    authen: function() {
+      return new Promise(async resolve => {
+        const userId = `${(Math.random() * 100000).toFixed(6)}`;
+        const userToken = await api.getUserToken(userId);
+        this.userToken = userToken;
+
+        if (!this.callClient) {
+          const client = new StringeeClient();
+
+          client.on("authen", function(res) {
+            console.log("on authen: ", res);
+            resolve(res);
+          });
+          this.callClient = client;
+        }
+        this.callClient.connect(userToken);
+      });
+    },
+    publish: async function(screenSharing = false) {
+      const localTrack = await StringeeVideo.createLocalVideoTrack(
+        this.callClient,
+        {
+          audio: true,
+          video: true,
+          screen: screenSharing,
+          videoDimensions: { width: 640, height: 360 }
+        }
+      );
+
+      const videoElement = localTrack.attach();
+      this.addVideo(videoElement);
+
+      const roomData = await StringeeVideo.joinRoom(
+        this.callClient,
+        this.roomToken
+      );
+      const room = roomData.room;
+      console.log({ roomData, room });
+
+      if (!this.room) {
+        this.room = room;
+        room.clearAllOnMethos();
+        room.on("addtrack", e => {
+          const track = e.info.track;
+
+          console.log("addtrack", track);
+          if (track.serverId === localTrack.serverId) {
+            console.log("local");
+            return;
+          }
+          this.subscribe(track);
+        });
+        room.on("removetrack", e => {
+          const track = e.track;
+          if (!track) {
+            return;
+          }
+
+          const mediaElements = track.detach();
+          mediaElements.forEach(element => element.remove());
+        });
+
+        // Join existing tracks
+        roomData.listTracksInfo.forEach(info => this.subscribe(info));
+      }
+
+      await room.publish(localTrack);
+      console.log("room publish successful");
+    },
+    createRoom: async function() {
+      const room = await api.createRoom();
+      const { roomId } = room;
+      const roomToken = await api.getRoomToken(roomId);
+
+      this.roomId = roomId;
+      this.roomToken = roomToken;
+      console.log({ roomId, roomToken });
+
+      await this.authen();
+      await this.publish();
+    },
+    join: async function() {
+      const roomToken = await api.getRoomToken(this.roomId);
+      this.roomToken = roomToken;
+
+      await this.authen();
+      await this.publish();
+    },
+    joinWithId: async function() {
+      const roomId = prompt("Dán Room ID vào đây nhé!");
+      if (roomId) {
+        this.roomId = roomId;
+        await this.join();
+      }
+    },
+    subscribe: async function(trackInfo) {
+      const track = await this.room.subscribe(trackInfo.serverId);
+      track.on("ready", () => {
+        const videoElement = track.attach();
+        this.addVideo(videoElement);
+      });
+    },
+    addVideo: function(video) {
+      video.setAttribute("controls", "true");
+      video.setAttribute("playsinline", "true");
+      videoContainer.appendChild(video);
+    }
+  }
+});
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+document.addEventListener('DOMContentLoaded',function(event){
+    // array with texts to type in typewriter
+    var dataText = [ "Video Conferencing Platform", "Coder: toidicodedao", "Upgrade: lttthedev", "APIs: Stringee"];
+    
+    // type one text in the typwriter
+    // keeps calling itself until the text is finished
+    function typeWriter(text, i, fnCallback) {
+      // chekc if text isn't finished yet
+      if (i < (text.length)) {
+        // add next character to h1
+       document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+  
+        // wait for a while and call this function again for next character
+        setTimeout(function() {
+          typeWriter(text, i + 1, fnCallback)
+        }, 90);
+      }
+      // text finished, call callback if there is a callback function
+      else if (typeof fnCallback == 'function') {
+        // call callback after timeout
+        setTimeout(fnCallback, 700);
+      }
+    }
+    // start a typewriter animation for a text in the dataText array
+     function StartTextAnimation(i) {
+       if (typeof dataText[i] == 'undefined'){
+          setTimeout(function() {
+            StartTextAnimation(0);
+          }, 20000);
+       }
+       // check if dataText[i] exists
+      if (i < dataText[i].length) {
+        // text exists! start typewriter animation
+       typeWriter(dataText[i], 0, function(){
+         // after callback (and whole text has been animated), start next text
+         StartTextAnimation(i + 1);
+       });
+      }
+    }
+    // start the text animation
+    StartTextAnimation(0);
+  });
+
